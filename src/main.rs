0ce5b7b049074e573d0782fba7f19a765387e69a
cadolphs <clemens.adolphs@gmail.com>
2022-11-30 18:21:10 +0000

    Adding crate to help with loading aoc data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 pub mod day01;
+use aocf::Aoc;
+use std::env;
 
 fn main() {
     day01::run_day_01();
+
+    let session_cookie = env::var("aocd_session_id").unwrap();
+
+    let mut aoc = Aoc::new()
+        .year(Some(2020))
+        .day(Some(2))
+        .cookie(&session_cookie)
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        println!("{}", i);
+    }
 }
