a4e97ed5e51be6af7904132b8997f8afbea5d339
cadolphs <clemens.adolphs@gmail.com>
2022-12-01 16:29:27 +0000

    Finish part 1 day 1.
    Refactor handle_block and make it run on the actual puzzle input.

--- src/day01.rs
+++ src/day01.rs
@@ -1,9 +1,22 @@
 pub fn run_day_01(input: String) {
-    println!("Running day one with input {}", input);
+    let blocks = input.trim().split("\n\n");
+    let sums = blocks.map(handle_block);
+
+    let max_sum = sums.max().unwrap();
+    println!("The elf with the most calories has {} calories.", max_sum);
+}
+
+fn handle_block(block: &str) -> i32 {
+    block
+        .split('\n')
+        .map(|num_as_str| num_as_str.parse::<i32>().unwrap())
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::day01::handle_block;
+
 
     #[test]
     fn hacking_around() {
@@ -12,12 +25,7 @@ mod tests {
         let mut split_on_blanks = input.split("\n\n");
         assert_eq!(3, split_on_blanks.clone().count());
 
-        fn handle_block(block: &str) -> i32 {
-            block
-                .split('\n')
-                .map(|num_as_str| num_as_str.parse::<i32>().unwrap())
-                .sum()
-        }
+        
 
         let sum_iterator = split_on_blanks.map(handle_block);
         assert_eq!(5, sum_iterator.max().unwrap());
