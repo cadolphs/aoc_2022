824c2cd7c7fb4bd8fa01d5eb7cca31800bd1dcdb
cadolphs <clemens.adolphs@gmail.com>
2022-12-01 16:25:21 +0000

    Quick test-based hacking for day 1

--- docs/day01.md
+++ docs/day01.md
@@ -5,3 +5,7 @@ very simple transformation. In Rust, I'd like to use _iterators_ for that purpos
 So, by splitting the input on a blank line, we'd get an iterator over the "chunks" of each elf. By splitting 
 _those_ chunks, we get the calories per elf. Then we're asking for the max over the sum. We can hack this together 
 at first, and then do a cleaner job with iterators and structures. Let's go!
+
+Okay. Inside the test module of day 1 I just hack around (and set the tests to run automatically on change via 
+`cargo watch -w src/ -x test`) and just play around with chaining iterators. Seems to work, so now I can just write 
+the code properly.
--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,25 @@
 pub fn run_day_01(input: String) {
     println!("Running day one with input {}", input);
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn hacking_around() {
+        let input = "1\n\n2\n3\n\n4\n".trim().to_string();
+
+        let mut split_on_blanks = input.split("\n\n");
+        assert_eq!(3, split_on_blanks.clone().count());
+
+        fn handle_block(block: &str) -> i32 {
+            block
+                .split('\n')
+                .map(|num_as_str| num_as_str.parse::<i32>().unwrap())
+                .sum()
+        }
+
+        let sum_iterator = split_on_blanks.map(handle_block);
+        assert_eq!(5, sum_iterator.max().unwrap());
+    }
+}
